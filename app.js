b84fc8d4d7c79eec50178a1c7d2d5c8b8b0798b1
Theron Kelso <theron.kelso@gmail.com>
2014-12-18 10:20:39 -0500

    completed hellobot.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 var express = require('express');
 var bodyParser = require('body-parser');
+var hellobot = require('./hellobot')
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get('/', function (req,res) {
   res.status(200).send('Hello world!');
 });
+app.post('/hello', hellobot);
 
 //error handler
 app.use(function (err, req, res, next){
--- hellobot.js
+++ hellobot.js
@@ -1,1 +1,26 @@
 //hellobot.js
+
+// Example data from slack
+// token=SnyOVGyaIcAC7AWDs3uquhdb
+// team_id=T0001
+// channel_id=C2147483705
+// channel_name=test
+// timestamp=1355517523.000005
+// user_id=U2147483697
+// user_name=Steve
+// text=googlebot: What is the air-speed velocity of an unladen swallow?
+// trigger_word=googlebot:
+
+module.exports = function (req, res, next){
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello, ' + userName + '!'
+  };
+
+  //avoid and ininite loop
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+};
