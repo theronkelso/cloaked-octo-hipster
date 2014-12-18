2373461482b13600753f7839aa099ebcbac9f058
Theron Kelso <theron.kelso@gmail.com>
2014-12-18 14:58:24 -0500

    include firebase

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //app.js
 var express = require('express');
 var bodyParser = require('body-parser');
-var hellobot = require('./hellobot')
+var hellobot = require('./hellobot');
+var firebase = require('firebase');
+var fb = new firebase('https://amber-inferno-3633.firebaseio.com/users');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -11,13 +13,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 //test route
 app.get('/', function (req,res) {
-  res.status(200).send('Hello world!');
+  //  fb.child('name').once('value', function(snapshot){
+  fb.once('value', function(snapshot){
+    console.log(snapshot.val());
+    res.status(200).send(snapshot.val());
+    //res.end();
+  }, function (errorObject){
+    console.log('the read failed: ' + errorObject.code);
+  });
 });
 app.post('/hello', hellobot);
 
 //error handler
 app.use(function (err, req, res, next){
-  console(error.stack);
+  console.log(err.stack);
   res.status(400).send(err.message);
 });
 
--- hellobot.js
+++ hellobot.js
@@ -1,3 +1,7 @@
+var firebase = require('firebase');
+
+var USERS_LOCATION = 'https://amber-inferno-3633.firebaseio.com/users';
+
 //hellobot.js
 
 // Example data from slack
@@ -11,8 +15,34 @@
 // text=googlebot: What is the air-speed velocity of an unladen swallow?
 // trigger_word=googlebot:
 
+function userCreated(userName, success){
+  if (!success){
+    console.log('user ' + userName + ' already exists!');
+  } else {
+    console.log('Successfully created ' + userName);
+  }
+}
+
+function tryCreateUser(userName, userData){
+  var usersRef = new firebase(USERS_LOCATION);
+
+  usersRef.child(userName).transaction(function(currentUserData){
+    console.log('in try');
+    if (currentUserData === null){
+
+      return userData;
+    }
+  }, function(error, commited) {
+    userCreated(userName, commited);
+  });
+}
+
 module.exports = function (req, res, next){
   var userName = req.body.user_name;
+  var userData = { name : req.body.user_name };
+
+  tryCreateUser(userName, userData);
+
   var botPayload = {
     text : 'Hello, ' + userName + '!'
   };
